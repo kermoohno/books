78016bdc80f9b1bdfdd89b3df352130cef41a993
Kermo <kermo.ohno@voco.com>
2022-10-27 13:23:45 +0300

    Add book to ls

--- app.js
+++ app.js
@@ -52,5 +52,15 @@ function deleteAllBooks(event) {
     }
 }
 
+function addBookToLS(book) {
+    let books
+    if(localStorage.getItem('books') === null) {
+        books = []
+    } else {
+        books = JSON.parse(localStorage.getItem('books'))
+    }
+    books.push(book)
+    localStorage.setItem('books', JSON.stringify(books))
+}
 
 
